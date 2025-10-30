968acbf583a6d5afbdd4d6a65278cc96bf2b22a7
Iker Vera <iker0004@gmail.com>
2025-10-30 19:30:13 +0100

    Catches nuevos y propios

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,46 @@
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.time.temporal.ChronoUnit;
-import java.util.Formatter;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     final static Scanner sc =  new Scanner(System.in);
+
     public static LocalDate fechaNacimiento;
     public static Period edad;
 
     public static int num1, num2;
+
+    public static StringBuilder frase = new StringBuilder();
+
     public static void main(String[] args) {
 
-
-        opciones();
+        try {
+            opciones();
+        }
+        catch (Exception e){
+            System.out.println("Errores: " + e.getClass());
+        }
 
     }
 
-    public static void opciones(){
+    //Panel de opciones
+    public static void opciones () throws Exception{
 
         System.out.print("""
-                -- Funciones del programa --
+                -- Menú de opciones --
                 a) Calcular la edad de una persona
                 b) Números pares y primos
                 c) Palíndromo
                 d) Salir
-                Que quieres hacer:
                 """);
+        System.out.print("Que quieres hacer: ");
 
         String opcion = sc.nextLine();
         switch (opcion){
@@ -42,22 +51,40 @@ public class Main {
                 numeroParPrimo();
                 break;
             case "c":
-              //  comprobarPalindromo();
+                pedirPalindromo();
                 break;
             case "d":
-               // finPrograma();
+                finPrograma();
                 break;
             default:
-                System.out.println("La opción no es valida");
+                System.out.println("** La opción no es valida **");
+                opciones();
         }
     }
 
-    public static void pedirEdad(){
-        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        System.out.print("Cuando naciste (DD/MM/YYYY): ");
-        fechaNacimiento = LocalDate.parse(sc.next(), formato);
-        edadPersona();
-        proximoCumpleanos();
+    //Apartado de edades A
+    public static void pedirEdad() throws Exception{
+        try {
+            DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            System.out.println("-----------------------------------");
+            System.out.print("Cuando naciste (DD/MM/YYYY): ");
+            fechaNacimiento = LocalDate.parse(sc.next(), formato);
+            if (fechaNacimiento.isAfter(LocalDate.now())){
+                throw new FechaMayorHoy();
+            }
+            edadPersona();
+            proximoCumpleanos();
+            sc.nextLine();
+            preguntarFin();
+        }
+        catch (DateTimeParseException ex){
+            System.out.println("** Los valores introducidos no son validos **");
+            pedirEdad();
+        }
+        catch (FechaMayorHoy ex){
+            System.out.println("** La fecha introducida no puede ser posterior a la fecha actual **");
+            pedirEdad();
+        }
     }
     public static void edadPersona(){
         edad = Period.between(fechaNacimiento, LocalDate.now());
@@ -70,18 +97,33 @@ public class Main {
         System.out.println("Tu siguiente cumpleaños es en " + proximo + " dias");
     }
 
-    public static void numeroParPrimo(){
+    //Apartado de numeros B
+    public static void numeroParPrimo() throws Exception{
 
-        System.out.println("-----------------------------------");
-        System.out.print("Escribe el primer numero: ");
-        num1 = sc.nextInt();
-        System.out.print("Escribe el segundo numero: ");
-        num2 = sc.nextInt();
+        try {
+            System.out.println("-----------------------------------");
+            System.out.print("Escribe el primer numero: ");
+            num1 = sc.nextInt();
+            System.out.print("Escribe el segundo numero: ");
+            num2 = sc.nextInt();
 
-        numerosEnMedio();
-        parPrimo();
+            if (num1 > num2){
+                throw new NumeroMayorAnterior();
+            }
+
+            numerosEnMedio();
+            parPrimo();
+            sc.nextLine();
+            preguntarFin();
+        }
+        catch (NumeroMayorAnterior ex){
+            System.out.println("** El primer numero no puede ser mayor que el segundo **");
+            numeroParPrimo();
+        }
+        catch (InputMismatchException ex){
+            System.out.println("** Valores no validos **");
+        }
     }
-
     public static void numerosEnMedio(){
 
         StringBuilder numerosImprimir = new StringBuilder();
@@ -90,31 +132,94 @@ public class Main {
             numerosImprimir.append(i).append(" ");
         }
 
-        System.out.printf("""
-                -----------------------------------
-                Los números que están entre %s y %s son:
-                %s""", num1, num2, numerosImprimir);
+        System.out.println("-- Números entre " + num1 + " Y " +  num2 + " --");
+        System.out.println(numerosImprimir);
     }
+    public static void parPrimo() {
 
-    public static void parPrimo(){
+        int numeroComprobar = num1;
+        int vueltas = 0;
 
-        if(num1 % 2 == 0){
-            System.out.print(num1 + " Es PAR");
-        }
-        else {
-            int contador = 0;
-            for(int i = 0; i < num1; i++){
-                if (i % num1 == 0){
-                    contador++;
+        System.out.println("-- Comparación de par/impar y primo --");
+
+        do {
+
+            if (numeroComprobar % 2 == 0) {
+
+                System.out.println("Numero " + numeroComprobar);
+                System.out.println("- Es par");
+
+            } else {
+                int contador = 0;
+                for (int i = 2; i <= num1; i++) {
+
+                    if (num1 % i == 0) {
+                        contador++;
+                    }
+                }
+                System.out.println("Numero " + numeroComprobar);
+                if (contador == 1) {
+                    System.out.println("- Es impar y es primo");
+                } else {
+                    System.out.println("- Es impar y no es primo");
                 }
             }
-            if(contador == 2){
-                System.out.printf("""
-                        El numero %s
-                            - Es IMPAR
-                            - Es PRIMO
-                        """);
-            }
+
+            numeroComprobar = num2;
+            vueltas++;
+
+        }while (vueltas < 2);
+    }
+
+    //Apartado de palíndromo C
+    public static void pedirPalindromo() throws Exception{
+        Pattern patron = Pattern.compile("^.[A-Za-z ]+");
+        System.out.println("-----------------------------------");
+        System.out.print("Escribe una frase para comprobar si es un palíndromo: ");
+        frase = frase.append(sc.nextLine().toLowerCase());
+        Matcher matcher = patron.matcher(frase);
+        if (!matcher.matches()){
+            System.out.println("** La frase solo puede tener letras en mayúsculas y minuscules **");
+            pedirPalindromo();
         }
+        comprobarPalindromo();
+        preguntarFin();
+    }
+    public static void comprobarPalindromo(){
+        String fraseComprobar = frase.toString().replaceAll(" ", "");
+
+        String alReves = frase.reverse().toString().replaceAll(" ","");
+
+
+        if (fraseComprobar.equals(alReves)){
+            System.out.println("La frase SI es un palíndromo");
+        } else {
+            System.out.println("La frase NO es un palíndromo");
+        }
+
+        frase.delete(0, frase.length());
+    }
+
+    public static void preguntarFin () throws Exception{
+
+        System.out.println("-----------------------------------");
+        System.out.print("Quires volver a las opciones (si/no): ");
+        String respuesta = sc.nextLine().toLowerCase();
+
+        if (respuesta.equals("si")){
+            System.out.println("-----------------------------------");
+            opciones();
+        } else if (respuesta.equals("no")){
+            finPrograma();
+        } else {
+            System.out.println("** Respuesta no valida **");
+            preguntarFin();
+        }
+    }
+
+    public static void finPrograma(){
+        System.out.println("-----------------------------------");
+        System.out.println("Fin del programa");
+
     }
 }
